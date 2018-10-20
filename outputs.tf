89b8ec298761db05a2525b1dba7eb48477d9358b
Eimert Vink <eimertvink@gmail.com>
2018-10-20 23:06:34 +0200

    outputs.tf fix

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
-output dns_name {
+output "dns_name" {
   description = "Output name for gc managed dns zone"
-  value = "{google_dns_managed_zone.name}"
+  value = "${google_dns_managed_zone.managed_zone.name}"
 }
 
-output dns_zone {
+output "dns_zone" {
   description = "Example output: prod.example.com."
-  value = "{google_dns_managed_zone.dns_name}"
+  value = "${google_dns_managed_zone.managed_zone.dns_name}"
 }
